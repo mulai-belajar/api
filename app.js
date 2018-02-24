e57945f4c427cd368e6ad16d425f7d01a5a31cc7
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-02-24 19:24:11 +0700

    Initialize express files

--- app.js
+++ app.js
@@ -1,69 +1,42 @@
-require("dotenv").config()
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-const cors = require("cors")
-const express = require('express')
-const path = require('path')
-const favicon = require('serve-favicon')
-const logger = require('morgan')
-const cookieParser = require('cookie-parser')
-const bodyParser = require('body-parser')
+var index = require('./routes/index');
+var users = require('./routes/users');
 
-const user = require('./routes/user')
-const admin = require('./routes/admin')
-const classes = require('./routes/class')
-const category = require('./routes/category')
-//const donation = require('./routes/donation')
+var app = express();
 
-const mongoose = require(`mongoose`)
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-const app = express()
-
-const db = mongoose.connection
-const url = `mongodb://localhost/mulaibelajar`
-const successMessage = `You're connected to MongoDB`
-const errorMessage = `Connection error : `
-
-// view engine setup
-
-app.use(cors())
-app.use(logger('dev'))
-app.use(express.static(path.join(__dirname, 'public', 'favicon.ico')))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({
-  extended: false
-}))
-app.use(cookieParser())
-
-app.use('/admin', admin)
-app.use('/user', user)
-app.use('/class', classes)
-app.use('/category', category)
-//app.use('/donation', donation)
-
-mongoose.connect(url);
-db.on(`error`, console.log.bind(console, errorMessage));
-db.once(`open`, () => {
- console.log(successMessage);
-});
+app.use('/', index);
+app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
 
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-
-  res.locals.message = err.message
-  res.locals.error = req.app.get('env') === 'development' ? err : {}
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500)
-  res.send(err)
-})
+  res.status(err.status || 500);
+  res.render('error');
+});
 
-module.exports = app
+module.exports = app;
