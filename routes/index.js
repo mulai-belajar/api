00dac62889f2122437e20b248c34fabaf405c592
Agtamas Miftahul <agtamasmiftahul37@gmail.com>
2018-02-24 20:52:29 +0700

    Add auth0 authorization

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const authService = require('../service/auth')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.send({ title: 'Express' });
-});
+// GET all data
+router.get('/public', (req,res) => {
+  res.json({
+    messages: 'Get public data'
+  })
+})
+
+router.get('/private', authService.checkJwt, (req,res) => {
+  res.json({
+    messages : 'Get private data. Congratulations'
+  })
+})
+
+router.get('/private-scopes', authService.checkJwt, authService.checkScopes, (req,res) => {
+  res.json({
+    messages : 'Get private data with scopes. Congratulations'
+  })
+})
 
 module.exports = router;
---
+++ service/auth.js
@@ -0,0 +1,23 @@
+const jwt = require('express-jwt')
+const jwtAuthz = require('express-jwt-authz')
+const jwksRsa = require('jwks-rsa')
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: 'https://mulaibelajar.auth0.com/.well-known/jwks.json'
+  }),
+
+  audience: 'http://api.mulaibelajar.com',
+  issuer: 'https://mulaibelajar.auth0.com/',
+  algorithms: ['RS256']
+})
+
+const checkScopes = jwtAuthz(['read:messages'])
+
+module.exports = {
+  checkJwt,
+  checkScopes
+}
